fixed the problem of min-max normalization aggregation

src/main.py
@@ -17,17 +17,17 @@ def main(experiment: str, setup: str, federated: bool, test: bool):
               + (' TESTING' if test else ' GRID SEARCH') + '\n', bold=True)
 
     common_params = {'n_features': 115,
-                     'normalization': 'min-max',
+                     'normalization': 'min-max',  # "min-max", "0-mean 1var"
                      'test_bs': 4096,
                      'p_test': 0.2,
                      'p_unused': 0.01,
                      'val_part': None,  # This is the proportion of training data that goes into the validation set, not the proportion of all data
-                     'n_splits': 5,
+                     'n_splits': 1,
                      'n_random_reruns': 1,
                      'cuda': False,  # It looks like cuda is slower than CPU for me so I enforce using the CPU
                      'benign_prop': 0.95,
                      # Desired proportion of benign data in the train/validation sets (or None to keep the natural proportions)
-                     'samples_per_device': 10_000}  # Total number of datapoints (train & val + unused + test) for each device.
+                     'samples_per_device': 100_000}  # Total number of datapoints (train & val + unused + test) for each device.
 
     # p_test, p_unused and p_train_val are the proportions of *all data* that go into respectively the test set, the unused set and the train &
     # validation set.
@@ -77,8 +77,8 @@ def main(experiment: str, setup: str, federated: bool, test: bool):
                                       'optimizer_params': {'lr': 1.0, 'weight_decay': 1e-5},
                                       'lr_scheduler': torch.optim.lr_scheduler.StepLR,
                                       'lr_scheduler_params': {'step_size': 20, 'gamma': 0.5}}
-                                      # 'lr_scheduler': torch.optim.lr_scheduler.ReduceLROnPlateau,
-                                      # 'lr_scheduler_params': {'patience': 3, 'threshold': 0.025, 'factor': 0.5, 'verbose': False}}
+    # 'lr_scheduler': torch.optim.lr_scheduler.ReduceLROnPlateau,
+    # 'lr_scheduler_params': {'patience': 3, 'threshold': 0.025, 'factor': 0.5, 'verbose': False}}
 
     classifier_opt_default_params = {'epochs': 4,
                                      'train_bs': 64,
@@ -144,15 +144,15 @@ def main(experiment: str, setup: str, federated: bool, test: bool):
                 constant_params.update(federation_params)
 
             # set the hyper-parameters specific to each configuration (overrides the parameters defined in constant_params)
-            configurations_params = [{'optimizer_params': {'lr': 1.0, 'weight_decay': 0.0}, 'hidden_layers': [115, 58, 29]},
-                                     {'optimizer_params': {'lr': 1.0, 'weight_decay': 0.0}, 'hidden_layers': [115, 58, 29]},
-                                     {'optimizer_params': {'lr': 1.0, 'weight_decay': 0.0}, 'hidden_layers': [115, 58, 29]},
-                                     {'optimizer_params': {'lr': 1.0, 'weight_decay': 0.0}, 'hidden_layers': [115, 58, 29]},
-                                     {'optimizer_params': {'lr': 1.0, 'weight_decay': 0.0}, 'hidden_layers': [115, 58]},
-                                     {'optimizer_params': {'lr': 1.0, 'weight_decay': 1e-05}, 'hidden_layers': [115, 58, 29]},
-                                     {'optimizer_params': {'lr': 1.0, 'weight_decay': 0.0}, 'hidden_layers': [115, 58, 29]},
-                                     {'optimizer_params': {'lr': 1.0, 'weight_decay': 1e-05}, 'hidden_layers': [115, 58, 29]},
-                                     {'optimizer_params': {'lr': 1.0, 'weight_decay': 0.0}, 'hidden_layers': [115, 58, 29]}]
+            configurations_params = [{'optimizer_params': {'lr': 0.5, 'weight_decay': 0.0001}, 'hidden_layers': [115, 58, 29]},
+                                     {'optimizer_params': {'lr': 0.5, 'weight_decay': 0.0}, 'hidden_layers': [115, 58, 29]},
+                                     {'optimizer_params': {'lr': 0.5, 'weight_decay': 0.0}, 'hidden_layers': [115, 58, 29]},
+                                     {'optimizer_params': {'lr': 0.5, 'weight_decay': 0.0}, 'hidden_layers': [115, 58, 29]},
+                                     {'optimizer_params': {'lr': 0.5, 'weight_decay': 0.0}, 'hidden_layers': [115, 58, 29]},
+                                     {'optimizer_params': {'lr': 0.5, 'weight_decay': 0.0}, 'hidden_layers': [115, 58, 29]},
+                                     {'optimizer_params': {'lr': 0.5, 'weight_decay': 0.0}, 'hidden_layers': [115, 58, 29]},
+                                     {'optimizer_params': {'lr': 0.5, 'weight_decay': 0.0}, 'hidden_layers': [115, 58, 29]},
+                                     {'optimizer_params': {'lr': 0.5, 'weight_decay': 0.0}, 'hidden_layers': [115, 58, 29]}]
 
             test_hyperparameters(all_data, setup, experiment, federated, splitting_function, constant_params, configurations_params, configurations)
         else:
@@ -198,3 +198,7 @@ if __name__ == "__main__":
         Ctp.set_max_depth(args.max_depth)  # Set the max depth at which we print in the console
 
     main(args.experiment, args.setup, args.federated, args.test)
+
+# TODO: maybe there's a problem with federated averaging for the normalization values: for min-max normalization it should actually
+#  select the min and max values of the clients instead of the avg / median / etc...
+#  Also, maybe that should happen before any training occurs